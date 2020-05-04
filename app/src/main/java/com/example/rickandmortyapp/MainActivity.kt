2ba2a5fd20015b12d4ac381baca1291ad3042294
Larissa Marães <larissa.maraes@involves.com>
2020-05-04 19:59:29 -0300

    init project

---
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.rickandmortyapp
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rickandmortyapp.databinding.ActivityMainBinding
+import com.example.rickandmortyapp.ui.main.SectionsPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+    }
+}
