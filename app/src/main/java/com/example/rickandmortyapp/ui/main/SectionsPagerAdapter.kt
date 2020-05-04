0fb310527674130fefb1b2079aeee7cdaee264ba
Larissa Marães <larissa.maraes@involves.com>
2020-05-04 20:11:56 -0300

    adding new tabs

--- app/src/main/java/com/example/rickandmortyapp/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,9 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.rickandmortyapp.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_characters,
+    R.string.tab_text_locations,
+    R.string.tab_text_episodes
 )
 
 /**
@@ -16,7 +17,7 @@ private val TAB_TITLES = arrayOf(
  * one of the sections/tabs/pages.
  */
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
@@ -29,7 +30,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return TAB_TITLES.size
     }
 }
