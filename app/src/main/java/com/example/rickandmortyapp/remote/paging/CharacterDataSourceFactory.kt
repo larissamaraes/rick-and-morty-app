1d8bd4cb471f301fd60b41a508d597feebebe8c7
Larissa Marães <larissa.maraes@involves.com>
2020-05-11 18:33:27 -0300

    creating character data source

---
+++ app/src/main/java/com/example/rickandmortyapp/remote/paging/CharacterDataSourceFactory.kt
@@ -0,0 +1,13 @@
+package com.example.rickandmortyapp.remote.paging
+
+import androidx.paging.DataSource
+import com.example.rickandmortyapp.model.Character
+import com.example.rickandmortyapp.remote.ApiService
+import kotlinx.coroutines.CoroutineScope
+
+class CharacterDataSourceFactory(
+    private val coroutineScope: CoroutineScope,
+    private val apiService: ApiService
+) : DataSource.Factory<Int, Character>() {
+    override fun create(): DataSource<Int, Character> = CharactersDataSource(coroutineScope, apiService)
+}
---
+++ app/src/main/java/com/example/rickandmortyapp/remote/paging/CharactersDataSource.kt
@@ -0,0 +1,43 @@
+package com.example.rickandmortyapp.remote.paging
+
+import androidx.paging.PageKeyedDataSource
+import com.example.rickandmortyapp.model.Character
+import com.example.rickandmortyapp.remote.ApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+class CharactersDataSource(
+    private val coroutineScope: CoroutineScope,
+    private val apiService: ApiService
+) : PageKeyedDataSource<Int, Character>() {
+    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Character>) {
+        createCoroutine(0, 1, callback, null)
+    }
+
+    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Character>) {
+        createCoroutine(params.key, params.key + 1, null, callback)
+    }
+
+    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Character>) {
+        createCoroutine(params.key, params.key - 1, null, callback)
+    }
+
+    override fun invalidate() {
+        super.invalidate()
+        coroutineScope.cancel()
+    }
+
+    private fun createCoroutine(
+        requestedPage: Int,
+        adjacentPage: Int,
+        initialCallback: LoadInitialCallback<Int, Character>?,
+        callback: LoadCallback<Int, Character>?
+    ) {
+        coroutineScope.launch {
+            val result = apiService.getCharacters(requestedPage)
+            initialCallback?.onResult(result.characters, null, adjacentPage)
+            callback?.onResult(result.characters, adjacentPage)
+        }
+    }
+}
