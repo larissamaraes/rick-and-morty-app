5c1e541fc46803d67e01d63e73ea481d47873d4e
Larissa Marães <larissa.maraes@involves.com>
2020-05-11 18:32:24 -0300

    changing api service to receive page as uri param

--- app/src/main/java/com/example/rickandmortyapp/remote/ApiService.kt
+++ app/src/main/java/com/example/rickandmortyapp/remote/ApiService.kt
@@ -1,11 +1,11 @@
 package com.example.rickandmortyapp.remote
 
 import com.example.rickandmortyapp.model.CharacterResponse
-import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("character/")
-    suspend fun getCharacters(): CharacterResponse
+    suspend fun getCharacters(@Query("page") page: Int): CharacterResponse
 }
