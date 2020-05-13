b0894b79f73fb2e555b6c5ecc8f23b94ed4a5ed2
Larissa Marães <larissa.maraes@involves.com>
2020-05-13 17:53:49 -0300

    adding get location request to api service

--- app/src/main/java/com/example/rickandmortyapp/remote/ApiService.kt
+++ app/src/main/java/com/example/rickandmortyapp/remote/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmortyapp.remote
 
 import com.example.rickandmortyapp.model.CharacterResponse
+import com.example.rickandmortyapp.model.LocationResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,4 +9,7 @@ interface ApiService {
 
     @GET("character/")
     suspend fun getCharacters(@Query("page") page: Int): CharacterResponse
+
+    @GET("location/")
+    suspend fun getLocations(@Query("page") page: Int): LocationResponse
 }
