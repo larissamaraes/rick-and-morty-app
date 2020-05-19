54e1a01aa18d398bd04d73cae5eb7923f5720d41
Larissa Marães <larissa.maraes@involves.com>
2020-05-19 18:29:31 -0300

    handlind DB build on create application

--- app/src/main/java/com/example/rickandmortyapp/RickAndMortyApplication.kt
+++ app/src/main/java/com/example/rickandmortyapp/RickAndMortyApplication.kt
@@ -3,14 +3,24 @@ package com.example.rickandmortyapp
 import android.app.Application
 import androidx.room.Room
 import com.example.rickandmortyapp.remote.AppDatabase
+import com.facebook.stetho.Stetho
 
 class RickAndMortyApplication : Application() {
-    val database: AppDatabase by lazy {
-        Room.databaseBuilder(
+
+    companion object {
+        var database: AppDatabase? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        database = Room.databaseBuilder(
             this,
             AppDatabase::class.java,
             "rick-and-morty-app-db"
-        )
-            .build()
+        ).build()
+        val initializerBuilder = Stetho.newInitializerBuilder(this)
+        initializerBuilder.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+        val initializer = initializerBuilder.build()
+        Stetho.initialize(initializer)
     }
 }
