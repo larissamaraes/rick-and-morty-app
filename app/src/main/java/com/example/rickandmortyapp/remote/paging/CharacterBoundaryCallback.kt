742090711541f0d3bd6db531682d0b04af62c60c
Larissa Marães <larissa.maraes@involves.com>
2020-05-21 18:30:16 -0300

    Create boundary callback

---
+++ app/src/main/java/com/example/rickandmortyapp/remote/paging/CharacterBoundaryCallback.kt
@@ -0,0 +1,42 @@
+package com.example.rickandmortyapp.remote.paging
+
+import android.util.Log
+import androidx.paging.PagedList
+import com.example.rickandmortyapp.RickAndMortyApplication
+import com.example.rickandmortyapp.dao.CharacterDao
+import com.example.rickandmortyapp.model.Character
+import com.example.rickandmortyapp.remote.api.ApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class CharacterBoundaryCallback (
+    private val apiService: ApiService
+) : PagedList.BoundaryCallback<Character>() {
+
+    private val characterDao: CharacterDao? by lazy {
+        RickAndMortyApplication.database?.characterDao()
+    }
+    private var lastRequestedPage = 1
+
+    override fun onZeroItemsLoaded() {
+        requestAndSaveData()
+    }
+
+    override fun onItemAtEndLoaded(itemAtEnd: Character) {
+        requestAndSaveData()
+    }
+
+    private fun requestAndSaveData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val result = apiService.getCharacters(lastRequestedPage)
+                characterDao?.insertCharacters(result.characters)
+                lastRequestedPage++
+            } catch (e: Exception) {
+                Log.d("[CHARACTER] ERROR PAGE:", lastRequestedPage.toString())
+            }
+        }
+    }
+}
