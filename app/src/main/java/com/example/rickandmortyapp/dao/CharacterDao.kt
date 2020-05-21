327773e1cb12177d9f29d959c8798410f0cf10b4
Larissa Marães <larissa.maraes@involves.com>
2020-05-21 18:32:23 -0300

    Getting characters from database

--- app/src/main/java/com/example/rickandmortyapp/dao/CharacterDao.kt
+++ app/src/main/java/com/example/rickandmortyapp/dao/CharacterDao.kt
@@ -1,12 +1,17 @@
 package com.example.rickandmortyapp.dao
 
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.rickandmortyapp.model.Character
 
 @Dao
 interface CharacterDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCharacters(characters: List<Character>)
+
+    @Query("SELECT * FROM character")
+    fun getCharacters(): DataSource.Factory<Int, Character>
 }
